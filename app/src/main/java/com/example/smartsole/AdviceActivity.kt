9d32c4922770eac3894e051178722ae8eaafdfcb
cbetsayda <60639986+cbetsayda@users.noreply.github.com>
2022-03-09 16:29:54 -0600

    first commit

---
+++ app/src/main/java/com/example/smartsole/AdviceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartsole
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdviceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advice)
+    }
+}
